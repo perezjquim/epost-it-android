d3b24702848bc10c5ef33670b164a82234c41caa
Joaquim Perez <jquim1@hotmail.com>
2019-11-10 19:18:10 +0000

    generic activity init

---
+++ app/src/main/java/com/perezjquim/epost_it/GenericActivity.java
@@ -0,0 +1,37 @@
+package com.perezjquim.epost_it;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.perezjquim.PermissionChecker;
+
+public class GenericActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.setTheme(R.style.AppTheme);
+        PermissionChecker.init(this);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            // tratamento das permissões
+            case PermissionChecker.REQUEST_CODE:
+                PermissionChecker.restart();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/perezjquim/epost_it/MainActivity.java
+++ app/src/main/java/com/perezjquim/epost_it/MainActivity.java
@@ -1,30 +1,14 @@
 package com.perezjquim.epost_it;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
 import com.perezjquim.PermissionChecker;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends GenericActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        super.setTheme(R.style.AppTheme);
-        PermissionChecker.init(this);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode)
-        {
-            // tratamento das permissões
-            case PermissionChecker.REQUEST_CODE:
-                PermissionChecker.restart();
-                break;
-        }
+        setContentView(R.layout.activity_main);
     }
 }
